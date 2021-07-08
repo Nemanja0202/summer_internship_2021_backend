30295ef4b3f2603b95efd3f1633c035a0e5b83f2
Nemanja Savkic <nemanja.savkic@rbt.rs>
2021-07-08 16:34:57 +0200

    Add /api prefix for endpoints

--- app/measurements/__init__.py
+++ app/measurements/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from flask_restx import Api
 
-measurement = Blueprint('measurement', __name__, url_prefix='/measurements')
+measurement = Blueprint('measurement', __name__,
+                        url_prefix='/api/measurements')
 
 measurement_api = Api(measurement)
 
