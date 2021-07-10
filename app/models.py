a442607a23e7add20491c6ae3d91d294de977e41
Nemanja Savkic <nemanja.savkic@rbt.rs>
2021-07-10 10:01:44 +0200

    Change air_quality to pollution in Measurement model

--- app/models.py
+++ app/models.py
@@ -6,13 +6,13 @@ class Measurement(db.Model):
     __tablename__ = 'measurements'
 
     id = db.Column(db.Integer, primary_key=True)
-    air_quality = db.Column(db.Float, nullable=False)
+    pollution = db.Column(db.Float, nullable=False)
     humidity = db.Column(db.Float, nullable=False)
     temperature = db.Column(db.Float, nullable=False)
     time_stamp = db.Column(db.DateTime(timezone=True), nullable=False,
                            default=datetime.utcnow)
 
-    def __init__(self, air_quality, humidity, temperature):
-        self.air_quality = air_quality
+    def __init__(self, pollution, humidity, temperature):
+        self.pollution = pollution
         self.humidity = humidity
         self.temperature = temperature
